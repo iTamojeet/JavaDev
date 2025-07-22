d1aec13faad3a184adac65ecf001d68ae4a50560
iTamojeet <bigcodingdinosaur@gmail.com>
2025-07-22 22:23:10 +0530

    initial commit :)

---
+++ CoreJava/LinearSearch.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class LinearSearch{
+    static int linearSearch(int[] arr, int target){
+        if(arr.length == 0){
+            return -1;
+        }
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == target)
+            return i;
+        }
+        return -1;
+    }
+    public static void main(String []args){
+        int []arr = {18,12,9,14,77,50};
+        int target = 15;
+        if(linearSearch(arr, target) == -1){
+            System.out.println("Not found!!!");
+        }
+        else{
+            System.out.println(target +" Found!!!");
+        }
+    }
+}
