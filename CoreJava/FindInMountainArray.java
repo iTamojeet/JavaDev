ee2f8d4fc5637c1ef976d8ae4fcb3c255209783d
iTamojeet <bigcodingdinosaur@gmail.com>
2025-08-02 19:45:11 +0530

    FindInMountainArray

--- CoreJava/FindInMountainArray.java
+++ CoreJava/FindInMountainArray.java
@@ -1,68 +1,75 @@
 import java.util.*;
 
-class FindInMountainArray{
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int peak = findPeakIndex(mountainArr);
 
-    static int search(int[] arr, int target){
-        int peak = peakIndexInMountainArray(arr);
-        int firstTry = orderAgnosticBinarySearch(arr, target , 0, peak);
-        if(firstTry != -1){
-            return firstTry;
-        }
-        // try to search in second half
-        return orderAgnosticBinarySearch(arr, target, peak+1, arr.length-1);
+        // Search in the increasing part
+        int firstTry = binarySearch(mountainArr, target, 0, peak, true);
+        if (firstTry != -1) return firstTry;
+
+        // Search in the decreasing part
+        return binarySearch(mountainArr, target, peak + 1, mountainArr.length() - 1, false);
     }
 
-    static int orderAgnosticBinarySearch(int[] arr, int target, int start, int end){
-
-        boolean isAsc = arr[start]<arr[end];
-
-        while(start <= end){
-            int mid = start + (end - start)/2;
-
-            if(arr[mid] == target){
-                return mid;
-            }
-
-            if(isAsc){
-                if(target < arr[mid])
-                end = mid - 1;
-                else
+    private int findPeakIndex(MountainArray mountainArr) {
+        int start = 0, end = mountainArr.length() - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (mountainArr.get(mid) > mountainArr.get(mid + 1)) {
+                end = mid;
+            } else {
                 start = mid + 1;
             }
-            else{
-                if(target > arr[mid])
-                end = mid - 1;
-                else
-                start = mid + 1;
+        }
+        return start;
+    }
+
+    private int binarySearch(MountainArray mountainArr, int target, int start, int end, boolean ascending) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int midVal = mountainArr.get(mid);
+
+            if (midVal == target) return mid;
+
+            if (ascending) {
+                if (target < midVal) end = mid - 1;
+                else start = mid + 1;
+            } else {
+                if (target < midVal) start = mid + 1;
+                else end = mid - 1;
             }
         }
         return -1;
     }
 
-    static int peakIndexInMountainArray(int[] arr){
-        int start = 0;
-        int end = arr.length - 1;
+    public static void main(String[] args) {
+        MountainArray mountainArr = new TestMountainArray(new int[]{1, 3, 5, 7, 6, 4, 2});
+        int target = 6;
+        Solution sol = new Solution();
+        int index = sol.findInMountainArray(target, mountainArr);
+        System.out.println("Target found at index: " + index);
+    }
+}
 
-        while(start < end){
-            int mid = start + (end - start)/2;
-            if(arr[mid] > arr[mid+1]){
-                /* We are in the descending part of the array
-                this might be hte answer, but to confirm 
-                look at the left
-                this is why end != mid - 1;
-                */
-               end = mid;
-            }
-            else{
-                // we r in the ascending part of the array
-                start = mid + 1; // because we know mid + 1 element > mid element
-            }
-        }
-        return start; // or return end as both are same... :)
+interface MountainArray {
+    public int get(int index);
+    public int length();
+}
+
+// Concrete implementation for testing
+class TestMountainArray implements MountainArray {
+    private int[] arr;
+
+    public TestMountainArray(int[] arr) {
+        this.arr = arr;
     }
 
-    public static void main(String []args){
-        int[] arr = {0,1,2,3,5,9,2,1,0};
-        System.out.println(arr[peakIndexInMountainArray(arr)]);
+    public int get(int index) {
+        return arr[index];
+    }
+
+    public int length() {
+        return arr.length;
     }
 }
