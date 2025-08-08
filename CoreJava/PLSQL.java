6f5a8b3783f77bbe94146140327a92168540ef8d
iTamojeet <bigcodingdinosaur@gmail.com>
2025-08-08 23:55:47 +0530

    jdbc with PLSQL

--- CoreJava/PLSQL.java
+++ CoreJava/PLSQL.java
@@ -10,6 +10,12 @@ class PLSQL{
 		
 		try(Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","c##scott","tiger")){
 			System.out.println("My con is "+con);
+			CallableStatement stmt = con.prepareCall("{call p1(?,?)}");
+			System.out.println("My statement is "+stmt);
+			stmt.setInt(1,Integer.parseInt(args[0]));
+			stmt.registerOutParameter(2,java.sql.Types.INTEGER);
+			stmt.executeUpdate();
+			System.out.println("The sal is "+stmt.getInt(2));
 		}
 		catch(SQLException e){
 			System.out.println(e);
