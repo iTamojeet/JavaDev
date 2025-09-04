826a44237a8b9e843232dbbecbaf580c1d70fa0b
iTamojeet <bigcodingdinosaur@gmail.com>
2025-09-04 14:27:25 +0530

    SciBo

--- C0reJava/projects/SciBo.java
+++ C0reJava/projects/SciBo.java
@@ -11,3 +11,40 @@ each simulating the roll of thre edice.
 a good estimate for the true theoretical win probability.
 */
 
+class SciBo{
+    public static void main(String []args){
+
+        // Constant number of trials
+        final int NUM_TRIALS = 1000000;
+
+        // Count the number of trials that result in a win
+        int numWins = 0;
+
+        // Iterate over the number of trials
+        for (int trial = 0; trial < NUM_TRIALS; trial++) {
+            // Simulate three dice
+            int die1 = (int) (Math.random() * 6) + 1;
+            int die2 = (int) (Math.random() * 6) + 1;
+            int die3 = (int) (Math.random() * 6) + 1;
+
+            // The bet loses on a triple
+            if (die1 == die2 && die2 == die3) {
+            continue;
+            }
+
+            // Otherwise, the bet wins on a sum greater than 10
+            else if (die1 + die2 + die3 > 10) {
+            numWins++;
+            }
+        }
+        /* 
+        Calculate the total fraction of wins
+        ***
+        Cast one value to a double to get a fractional result
+        */
+        double winPercentage = ((double) numWins) / NUM_TRIALS;
+
+        // Output
+        System.out.printf("Winning prob. = %.4f.", winPercentage);
+    }
+}
