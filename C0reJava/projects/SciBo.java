d469faf3b13ba9b07a857a853c2803daf82d86ea
iTamojeet <bigcodingdinosaur@gmail.com>
2025-09-04 14:08:50 +0530

    SciBo

---
+++ C0reJava/projects/SciBo.java
@@ -0,0 +1,13 @@
+// Estimate the winning probability of sic bo's big bet
+
+/*
+The following program uses a Monte Carlo approach to estimate 
+the probability of winning the big bet in sicbo...
+
+• Perform a large number of random trials,
+each simulating the roll of thre edice.
+• Keep acount of the number of trials that would win the big bet.
+• The fraction of simulated trials that result in a win should be
+a good estimate for the true theoretical win probability.
+*/
+
