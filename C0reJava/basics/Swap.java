433514cc759b82bd09f0fcc40dcdb537562e8c11
iTamojeet <bigcodingdinosaur@gmail.com>
2025-09-07 01:18:04 +0530

    swapping

---
+++ C0reJava/basics/Swap.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Swap{
+    public static void main(String ...args){
+        int a=10, b=20;
+        System.out.println("a: "+a+", b: "+b);
+
+        a = a^b;
+        b = a^b;
+        a = a^b;
+
+        System.out.println("a: "+a+", b: "+b);
+    }
+}
+
+/*
+Swapping two numbers using bit-wise XOR
+*/
