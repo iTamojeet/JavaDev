56f1b6a07d61d507b665cdde623da45e18a37430
iTamojeet <bigcodingdinosaur@gmail.com>
2025-09-13 00:08:18 +0530

    one to one

--- SpringBootProjects/OneToOne/src/main/java/com/bidirectional/OneToOneBidirectional.java
+++ SpringBootProjects/OneToOne/src/main/java/com/bidirectional/OneToOneBidirectional.java
@@ -3,6 +3,7 @@ package com.bidirectional;
 import com.bidirectional.entity.Address;
 import com.bidirectional.entity.Order;
 import com.bidirectional.repository.AddressRepository;
+import com.bidirectional.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,6 +13,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OneToOneBidirectional implements CommandLineRunner {
     @Autowired
     private AddressRepository addressRepository;
+    @Autowired
+    private OrderRepository orderRepository;
 
     public static void main(String[] args)  {
         SpringApplication.run(OneToOneBidirectional.class, args);
@@ -32,5 +35,13 @@ public class OneToOneBidirectional implements CommandLineRunner {
         address.setOrder(order);
 
         addressRepository.save(address); // saves both
+
+        System.out.println("-".repeat(1000));
+        Order order2 = orderRepository.findByOrderName("table");
+        System.out.println(order2.getAddress().getCity()+" "+order2.getAddress().getState());
+
+        System.out.println("-".repeat(1000));
+        Address ad=addressRepository.findById(1).get();
+        System.out.println(ad.getOrder().getOrderName()+" "+ad.getOrder().getOrderPrice());
     }
 }
