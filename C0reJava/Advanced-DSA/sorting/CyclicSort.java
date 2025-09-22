d175c393c615dd55c0442300f614b59f900db445
iTamojeet <bigcodingdinosaur@gmail.com>
2025-09-22 21:33:05 +0530

    cyclic sort

---
+++ C0reJava/Advanced-DSA/sorting/CyclicSort.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class CyclicSort{
+    public static void main(String ...args){
+        int[] arr = {3,1,5,4,2};
+        sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] arr){
+        int i=0;
+        while(i < arr.length){
+            int correctIndex = arr[i] - 1;
+            if(arr[i] != arr[correctIndex]){
+                swap(arr, i, correctIndex);
+            }
+            else{
+                i++;
+            }
+        }
+    }
+
+    static void swap(int[] arr, int i, int correctIndex){
+        int temp = arr[i];
+        arr[i] = arr[correctIndex];
+        arr[correctIndex] = temp;
+    }
+}
