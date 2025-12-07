e49a90e75a4ad9598e90ff774c8d5249228413b2
iTamojeet <bigcodingdinosaur@gmail.com>
2025-12-07 22:57:01 +0530

    recursion

---
+++ C0reJava/Advanced-DSA/recursion/Main.java
@@ -0,0 +1,24 @@
+class Main{
+    void main(){
+        message();
+    }
+    static void message(){
+        IO.println("Hello World");
+        message1();
+    }
+    static void message1(){
+        IO.println("Hello World");
+        message2();
+    }
+    static void message2(){
+        IO.println("Hello World");
+        message3();
+    }
+    static void message3(){
+        IO.println("Hello World");
+        message4();
+    }
+    static void message4(){
+        IO.println("Hello World");
+    }
+}
