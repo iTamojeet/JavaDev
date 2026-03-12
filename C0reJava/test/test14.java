f5e8e20a29f528120672b7ad3e046d17f66dd205
iTamojeet <bigcodingdinosaur@gmail.com>
2026-03-12 12:55:40 +0530

    testing core java

---
+++ C0reJava/test/test14.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class test14 {
+    
+    int s;
+    String n;
+    Double d[];
+    List<Boolean> lst = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+        // public, private and protected not possible before local variable.
+        // public int ha = 10;
+
+        // test14 test14=80;
+        test14 test14=null;
+        test14 _dhur=new test14();
+        System.out.println(test14);
+        System.out.println(_dhur);
+
+        double []dd[]={{1.0,2.0,3.0},{4.0,5.0,6.0}};
+    }
+}
+class A{
+    int o;
+
+    A(){}
+    A(int o){ this.o = o; }
+
+    public static void main(String[] args) {
+        A a = new A();
+        A aa = new A(0);
+        System.out.println(a.o);
+        System.out.println(aa.o);
+        System.out.println(a.o == aa.o);
+    }
+}
