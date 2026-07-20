e678e50e214aea012277e8c161d2191f924ef565
iTamojeet <bigcodingdinosaur@gmail.com>
2026-07-20 11:33:27 +0530

    learning

---
+++ C0reJava/test/test19.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Test19{
+    public static void main(String[] args) {
+        List<Object> list = new ArrayList<>();
+        Collection<Object> coll = new LinkedList<>();
+        coll=new Vector<>();
+        coll=new Stack<>();
+        coll=new PriorityQueue<>();
+        coll=new ArrayDeque<>();
+
+
+        Map<Object,Object> map = new TreeMap<>();
+        // map = new AbstractMap<>();
+        // map = new EnumMap<>();
+        map = new HashMap<>();
+    }
+}
