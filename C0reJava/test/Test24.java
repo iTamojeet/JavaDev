43081185346eb08a3ffd435bf27813f508b1a8a3
iTamojeet <bigcodingdinosaur@gmail.com>
2026-08-15 01:26:36 +0530

    onekkk kichu

--- C0reJava/test/Test24.java
+++ C0reJava/test/Test24.java
@@ -1,7 +1,15 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Test24 {
+class Test24 {
+
+    public Test24(){}
+    public Test24(double a){
+        IO.println("Constructor says: "+a);
+    }
+    private Test24(String a){
+        IO.println("Constructor says: "+a);
+    }
     public static void main(String[] args) {
         IO.println(Pattern.matches("[abc]*", "ddd"));
         IO.println(Pattern.matches("\\d", "1"));
@@ -27,5 +35,23 @@ public class Test24 {
         } else {
             IO.println("Invalid Indian phone number.");
         }
+
+        new Test24(-0);
+        new Test24("a");
+        new Test24('x');
+        IO.println(new Test24().hashCode());
+        IO.println(new Test24().hashCode());
+        IO.println("a".hashCode()=="a".hashCode());
+    }
+}
+
+class X{
+    static{
+        new Test24(-100);
+        // new Test24("-a");
+        new Test24('0');
+    }
+    public static void main(String[] args) {
+        
     }
 }
